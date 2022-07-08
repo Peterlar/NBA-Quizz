c40f740a9411ce2f6af4c8ed8294e10bca1cf735
Peterlar <Peter_kjaer@hotmail.com>
2022-07-08 16:21:51 +0000

    "update"

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 # ------------------
 def new_game():
-    pass
+    
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in question_num:
+        print(key)
 # ------------------
 def check_answer():
     pass
@@ -10,3 +16,18 @@ def display_score():
 # ------------------
 def play_again():
     pass
+
+
+questions = {
+    "Michael Jordan played with what number for Chicago Bulls?": ":"A",
+    "Wich team was Lebron James first team?": ":"B",
+    "What year was nba invented?": ":"D",
+    "Who won 2022 nba championship?" ":"D",
+}
+
+options = [["A. 23", "B. 24", "C. 25", "D. 26"],
+          ["A. Lakers", "B. Cleveland", "C. Miami", "D. Bucks"],
+          ["A. 1944", "B. 1945", "C. 1947", "D. 1946"],
+          ["A. Lakers","B. Boston", "C. Bulls", "D. Warriors"]]
+
+new_game()
